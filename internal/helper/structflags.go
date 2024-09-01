6a28c9a1317d5ad47c5aac9b2369043d8d5e14ea
Sergey Davydov <sddevelop@ya.ru>
2024-09-01 17:51:44 +0300

    fixes

--- internal/helper/structflags.go
+++ internal/helper/structflags.go
@@ -28,7 +28,6 @@ func GenerateFlags(dst interface{}, fs *pflag.FlagSet) (err error) {
 }
 
 func generateFlags(rv reflect.Value, fs *pflag.FlagSet) (err error) {
-
 	rt := rv.Type()
 	for i := 0; i < rt.NumField(); i++ {
 		sf := rt.Field(i)
@@ -36,7 +35,7 @@ func generateFlags(rv reflect.Value, fs *pflag.FlagSet) (err error) {
 		switch fv.Kind() {
 		case reflect.Struct:
 			err = generateFlags(fv, fs)
-		case reflect.Bool, reflect.Int64, reflect.Float64, reflect.Int, reflect.Uint64, reflect.String:
+		default:
 			tagNames := [2]string{}
 			copy(tagNames[:], strings.SplitN(sf.Tag.Get("flag"), ",", 2))
 			if tagNames[0] == "" {
@@ -44,7 +43,6 @@ func generateFlags(rv reflect.Value, fs *pflag.FlagSet) (err error) {
 			}
 			usage := sf.Tag.Get("usage")
 			defVal := sf.Tag.Get("default")
-
 			switch f := fv.Addr().Interface().(type) {
 			case *bool:
 				defVal, _ := strconv.ParseBool(defVal)
@@ -63,9 +61,9 @@ func generateFlags(rv reflect.Value, fs *pflag.FlagSet) (err error) {
 			case *uint64:
 				defVal, _ := strconv.ParseUint(defVal, 10, 64)
 				fs.Uint64VarP(f, tagNames[0], tagNames[1], defVal, usage)
+			default:
+				err = errors.New("unknown type")
 			}
-		default:
-			err = errors.New("unknown type")
 		}
 	}
 	return
