7a04f90492b4aaaeb7e572a82de695074f524c7c
Sergey Davydov <sddevelop@ya.ru>
2024-10-24 22:50:35 +0300

    pprof

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
-	"gophKeeper/internal/server/app"
 
+	"log"
+	"net/http"
 	_ "net/http/pprof"
+
+	"gophKeeper/internal/server/app"
 
 	_ "github.com/lib/pq"
 )
@@ -14,6 +17,9 @@ var buildDate string
 var buildCommit string
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil)) // Запустите сервер pprof
+	}()
 	app.RunApp(context.Background(), nil, nil,
 		app.BuildMetadata{Version: buildVersion, Date: buildDate, Commit: buildCommit})
 }
