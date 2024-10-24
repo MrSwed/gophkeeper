5e470c47107d642c10c107d76dd7baca21dafacf
Sergey Davydov <sddevelop@ya.ru>
2024-10-24 22:28:06 +0300

    Refactor version info output to multiline format

--- internal/client/cmd/root.go
+++ internal/client/cmd/root.go
@@ -9,9 +9,11 @@ Main functionalities include:
 - Displaying application information, including version and build date.
 - Setting up the root command and adding shell command support.
 */
+
 package cmd
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 
@@ -19,25 +21,39 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// shortVersionInfo returns a short version information string.
+func (a *app) shortVersionInfo() string {
+	return fmt.Sprintf("Version: %s", a.v.Version)
+}
+
+// fullVersionInfo returns a full version information string.
+func (a *app) fullVersionInfo() string {
+	return fmt.Sprintf("Version: %s\nBuild date: %s\nCommit: %s", a.v.Version, a.v.Date, a.v.Commit)
+}
+
 // addRootCmd adds the root command to the application.
 // The root command serves as the main entry point for the GophKeeper client,
 // providing information about the application and allowing access to subcommands.
 func (a *app) addRootCmd() *app {
-	appInfo := `Client for saving encrypted data.`
-	if a.v.Version != "" {
-		appInfo += ` Version: ` + a.v.Version
-	}
-	if a.v.Date != "" {
-		appInfo += `, build date: ` + a.v.Date
-	}
 	c := &cobra.Command{
 		Use: func() string {
 			_, file := filepath.Split(os.Args[0])
 			return file
 		}(),
 		Short: "GophKeeper client",
-		Long:  appInfo,
+		Long:  "Client for saving encrypted data.",
 	}
+
+	// Add a flag for displaying the full version information
+	c.Flags().BoolP("version", "v", false, "Display full version information")
+	c.Run = func(cmd *cobra.Command, args []string) {
+		if showVersion, _ := cmd.Flags().GetBool("version"); showVersion {
+			fmt.Println(a.fullVersionInfo())
+		} else {
+			fmt.Println(a.shortVersionInfo())
+		}
+	}
+
 	c.AddCommand(shell.New(c, nil))
 	a.root = c
 	return a
